761d65825eef2527ee95455e96541d4bf2120399
Karthik <kartisuri@gmail.com>
2017-10-11 10:12:41 +0800

    removed unnecessary files

--- bottry.js
+++
@@ -1,6 +0,0 @@
-var mitsuku = require('mitsuku-api')();
-var body = '';
-process.argv.forEach(function (val, index, array) {
-  if (index == 2){body = val;}
-});
-mitsuku.send(body).then(function(result){console.log(result);});
