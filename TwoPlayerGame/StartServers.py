bbafa5339ffea29ff82568ce2f03639b6410c132
Karthik <kartisuri@gmail.com>
2017-10-19 18:27:38 +0800

    added chatbot.js file

--- TwoPlayerGame/StartServers.py
+++ TwoPlayerGame/StartServers.py
@@ -3,9 +3,11 @@ from os.path import abspath, dirname, join
 
 directory = join(dirname(abspath(__file__)))
 base_command = r'start /B start cmd.exe @cmd /k '
-command1 = "pip3 install -U numpy tornado requests otree-core"
+command1 = "pip3 install -U numpy tornado requests otree-core && npm install -g pb-node"
 command2 = base_command + 'python "' + directory + r'"\LogServer.py'
-command3 = base_command + 'cd "' + directory + '"'
+command3 = base_command + 'node "' + directory + r'"\chatbot.js'
+command4 = base_command + 'cd "' + directory + '"'
 os.system(command1)
 os.system(command2)
 os.system(command3)
+os.system(command4)
