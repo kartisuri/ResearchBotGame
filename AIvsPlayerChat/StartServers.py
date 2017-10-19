eb02e41a5cc58804850cd2e2bf6f28942b0dbced
Karthik <kartisuri@gmail.com>
2017-10-19 11:03:13 +0800

    cleverbot chat added again for testing

--- AIvsPlayerChat/StartServers.py
+++ AIvsPlayerChat/StartServers.py
@@ -2,10 +2,10 @@ import os
 from os.path import abspath, dirname, join
 
 directory = join(dirname(abspath(__file__)))
-base_command = r"start /B start cmd.exe @cmd /k "
-command1 = base_command + "python " + directory + r"\LogServer.py"
-command2 = base_command + "node " + directory + r"\chatbot.js"
-command3 = "cd " + directory + " && otree.exe runserver"
+base_command = r'start /B start cmd.exe @cmd /k '
+command1 = base_command + 'python "' + directory + r'"\LogServer.py'
+command2 = base_command + 'node "' + directory + r'"\chatbot.js'
+command3 = base_command + 'cd "' + directory + '"'
 os.system(command1)
 os.system(command2)
 os.system(command3)
